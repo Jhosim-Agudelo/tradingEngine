a108ff6de414f5d5fb4cf6d93794e39ea293a162
Jhosim-agudelo <jhosim.alejandro.agudelo.campo@umontreal.ca>
2024-05-18 15:46:17 -0400

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jhosim-Agudelo/tradingEngine
+
+go 1.21.6
---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var intnum int 
+	fmt.Print(intnum)
+
+	var floatnum float32  = 23.36
+
+	fmt.Print(floatnum)	
+}
